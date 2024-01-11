5d06b6b81340e6b3e84a08ff97e6e02b2708fcf7
Salami Bashir <bashirsalami78@gmail.com>
2024-01-11 14:24:44 +0100

    feat(assignment): solution

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("actionButton").addEventListener("click", function() {
+document.getElementById("actionButton").addEventListener("click", function () {
+    toggleContent();
     alert("Button clicked!");
 });
+
+// JavaScript for dynamic date in the footer
+document.getElementById('currentYear').innerText = new Date().getFullYear();
+
+// JavaScript for content toggle
+function toggleContent() {
+    var content = document.getElementById('description');
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+}
